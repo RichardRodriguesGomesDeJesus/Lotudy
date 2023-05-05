6423bc4b36fa760dc24ccea70c76b2ff16725cf5
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-05 19:50:26 -0300

    update style components

--- components/sharedstyles.tsx
+++ components/sharedstyles.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components'
 
 export const colors = {
-  principal: "#16c1c8",
-  segundaria: "#49cccc",
-  terceira: "#7cd7cf",
-  verdePrincipal:"#aee1d3",
-  verdeSegundaria: "#e1ecd6"
+  principalColor: "#16c1c8",
+  sideColor: "#665df5",
+  titleColor: "#49cccc",
+  backgrondColor: "#f0f0f0",
+  white: "#fff",
+  textColor:"#767675"
 }
 
 const Container = styled.div`
+  font-family: 'Poppins', sans-serif;
   background: #f0f0f0;
   padding: 0 0.5rem;
   display: flex;
@@ -19,27 +21,75 @@ const Container = styled.div`
   min-height: 100vh;
 `
 const Main = styled.main`
-  padding: 5rem 1rem;
+  color: ${colors.textColor} ;
+  padding: 0 1rem;
   flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 2rem;
   width:100vw;
 `
 
 const Header = styled.header`
-  background: ${colors.principal} ;
+  background: ${colors.principalColor} ;
   display: flex;
   height: 5rem;
   width: 100vw;
+  nav{
+    align-items: center;
+    color: #fff;
+    display: flex;
+    margin: 0 1rem;
+    font-weight: 400;
+    width: 100%;
+  }
 `
 const Title = styled.h1`
-  color: ${colors.segundaria};
+  color: ${colors.titleColor};
+  font-weight: 600;
   text-align:center;
+  @media screen and (min-width: 0 ){
+    font-size: 1.25rem;
+  };
+  @media screen and (min-width: 768px ){
+    font-size: 1.5rem;
+  
+  };
+  @media screen and (min-width: 1024px) {
+    font-size: 2rem;
+  };
 ` 
 const Description = styled.p`
-
+display: flex;
+flex-grow: 1;
+align-items: center;
+text-align: center;
+margin:0;
+background: ${colors.white};
+font-weight: 400;
+width:100vw;
+@media screen and (min-width: 0 ){
+  font-size: 1rem;
+  padding: 1rem;
+};
+@media screen and (min-width: 768px ){
+  font-size: 1.25rem;
+  padding: 2rem;
+};
+@media screen and (min-width: 1024px) {
+  padding: 3rem;
+};
 `
-export { Container, Main, Header, Title, Description}
+const Footer = styled.footer`
+  color: ${colors.white};
+  justify-content: center;
+  align-items: center;
+  background: ${colors.sideColor};
+  font-weight: 400;
+  display: flex;
+  height: 80px;
+  text-align: center;
+  width: 100vw;
+`
+export { Container, Main, Header, Title, Description, Footer}
