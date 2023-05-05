9d1a02ec1397e8fc29f691821ef68c60ae3a0ad2
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-05 10:03:52 -0300

    update cards

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,49 +1,94 @@
-import styled from 'styled-components'
-import Link from 'next/link'
-
+import styled from 'styled-components';
+import { colors } from './sharedstyles';
+import { link } from 'fs';
 const FlexContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-flow: column wrap;
-  max-width: 800px;
-  margin-top: 3rem;
-`
+  gap: 1rem;
+  flex-flow: row wrap;
+  width: 80%;
+  margin: 1rem;
+
+  @media (max-width: 768px) {
+    flex-flow: column;
+    height: auto;
+  }
+`;
 
 const Card = styled.div`
-  padding: 1.5rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  text-align: center;
+  padding: 0;
   color: inherit;
   text-decoration: none;
   border: 1px solid black;
   border-radius: 10px;
   transition: color 0.15s ease, border-color 0.15s ease;
-  width: 100%;
 
+  p{
+    margin: 0;
+  }
   &:hover,
   :focus,
   :active {
-    color: #0070f3;
-    border-color: #0070f3;
+    cursor: pointer;
+    color: ${colors.principal};
+    border-color: ${colors.segundaria};
   }
-`
-
-const StyledA = styled.a`
-  margin: 0 0 1rem 0;
-  font-size: 1.5rem;
-`
-
-const StyledLink = ({ href, name }) => (
-  <Link href={href} passHref legacyBehavior>
-    <StyledA>{name}</StyledA>
-  </Link>
-)
+  @media screen and (min-width: 0 ){
+    height:150px;
+    width: 150px;
+    img{
+      height: 75px;  
+      width:  75px ;
+    }
+  };
+  @media screen and (min-width: 768px ){
+    height:200px;
+    width: 200px;
+    img{
+      height: 100px;  
+      width: 100px ;
+    }
+  };
+  @media screen and (min-width: 1024px) {
+    height:250px;
+    width: 250px;
+    img{
+      height: 150px;  
+      width: 150px;
+    }
+  };
+`;
 
 export default function Cards() {
+  const cardList = [
+    {
+      img: 'cards/studying.svg',
+      text: 'Estude onde quiser. ',
+    },
+    {
+      img: 'cards/winners.svg',
+      text: 'Conquiste a nota maxima no vestibular ',
+    },
+    {
+      img: 'cards/growth.svg',
+      text: 'Crie o habito de estudar de uma forma simples' ,
+    }
+  ];
   return (
     <FlexContainer>
-      <Card>
-        <StyledLink href="/about" name="About Page &rarr;" />
-      </Card>
+      {cardList.map((element, index) => (
+        <Card key={index}>
+          <img src={element.img} alt={element.text} />
+          <p>{element.text}</p>
+        </Card>
+      ))}
     </FlexContainer>
-  )
+  );
 }
--- components/sharedstyles.tsx
+++ components/sharedstyles.tsx
@@ -1,54 +1,45 @@
 import styled from 'styled-components'
 
+export const colors = {
+  principal: "#16c1c8",
+  segundaria: "#49cccc",
+  terceira: "#7cd7cf",
+  verdePrincipal:"#aee1d3",
+  verdeSegundaria: "#e1ecd6"
+}
+
 const Container = styled.div`
+  background: #f0f0f0;
   padding: 0 0.5rem;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: auto;
   min-height: 100vh;
 `
 const Main = styled.main`
-  padding: 5rem 0;
+  padding: 5rem 1rem;
   flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin: 2rem;
+  width:100vw;
 `
 
+const Header = styled.header`
+  background: ${colors.principal} ;
+  display: flex;
+  height: 5rem;
+  width: 100vw;
+`
 const Title = styled.h1`
-  margin: 0;
-  line-height: 1.15;
-  font-size: 4rem;
-  text-align: center;
-  text-decoration: none;
-
-  a {
-    color: ${({ theme }) => theme.colors.secondary};
-    text-decoration: none;
-    &:hover,
-    :focus,
-    :active {
-      text-decoration: underline;
-    }
-  }
-`
-
+  color: ${colors.segundaria};
+  text-align:center;
+` 
 const Description = styled.p`
-  text-align: center;
-  line-height: 1.5;
-  font-size: 1.5rem;
-`
-const CodeTag = styled.code`
-  background: #fafafa;
-  border-radius: 5px;
-  margin: 0 0.75rem;
-  padding: 0.75rem;
-  font-size: 1.1rem;
-  font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-    Bitstream Vera Sans Mono, Courier New, monospace;
-`
 
-export { Container, Main, Title, Description, CodeTag }
+`
+export { Container, Main, Header, Title, Description}
