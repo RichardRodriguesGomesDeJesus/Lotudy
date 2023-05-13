ea15e1b9d64056a15683d8d6ff91019fcddef96e
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-13 11:04:42 -0300

    update validator lib

--- pages/api/auth/login_user.js
+++ pages/api/auth/login_user.js
@@ -1,6 +1,8 @@
 import { connectMongo } from '../../../lib/connectMongo.js';
+import jwt from 'jsonwebtoken';
 import { UserModel } from "../../../models/user.js"
 import bcrypt from 'bcryptjs';
+import validator from 'validator';
 
 
 
@@ -9,19 +11,48 @@ export default async function handler(req, res) {
         return res.status(405).send({ mse: 'Method Not Allowed' });
       }
       const { email, password } = req.body;
-      await connectMongo();
-      const users = await UserModel.find({ });
-      const emails = users.map(user => user.email)
-      const existEmail = emails.includes(email)
-      if (existEmail === true) {
-        const index = emails.indexOf(email)
-        const passwordMatch = await bcrypt.compare(password, users[index].password);
-        if (passwordMatch) {
-          res.status(201).send('sucesso!');
+      if (!validator.isEmail(email)) {
+        return res.status(422).send({ mse: 'The email provided is invalid' });
+      }
+      if (!validator.isStrongPassword(password, {
+        minLength: 8,
+        maxLength: 20,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+      })) {
+        return res.status(422).send({ mse: 'Password must be at least 8 characters long, including uppercase and lowercase letters, numbers and symbols' });
+      }
+      if (!email) {
+        return res.status(422).send({ mse: 'email is required' });
+      }
+      if (!password) {
+        return res.status(422).send({ mse: 'password is required' });
+      }
+      try{
+        await connectMongo();
+        const users = await UserModel.find({ });
+        const emails = users.map(user => user.email)
+        const existEmail = emails.includes(email)
+        if (existEmail === true) {
+          const index = emails.indexOf(email)
+          const passwordMatch = await bcrypt.compare(password, users[index].password);
+          if (passwordMatch) {
+            const token = jwt.sign({
+              userId: newUser._id,
+              name: newUser.name,
+              email: newUser.email
+            }, process.env.SECRET)
+            res.status(201).send({mse:'successful authentication', token});
+          } else {
+            res.status(401).send('Incorrect password or email')
+          }
         } else {
-          res.status(401).send('senha ou email  incorretos')
+          res.status(401).send('User not found');
         }
-      } else {
-        res.status(401).send('senha ou email ');
+      } catch (error) {
+        console.error('Error connecting to database', error);
+        return res.status(500).send({ msg: 'An error occurred while processing the request' });
       }
 }
--- pages/api/auth/set_user.js
+++ pages/api/auth/set_user.js
@@ -1,12 +1,42 @@
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 import { connectMongo } from '../../../lib/connectMongo.js';
 import { UserModel } from "../../../models/user.js"
 import bcrypt from 'bcryptjs';
+import validator from 'validator';
+
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).send({ mse: 'Method Not Allowed' });
   }
+  
   const { name, email, password } = req.body;
+  if (!validator.isLength(name, { min: 2 })) {
+    return res.status(422).send({ mse: 'The name must be at least 2 characters long' });
+  }
+  if (!validator.isEmail(email)) {
+    return res.status(422).send({ mse: 'The email provided is invalid' });
+  }
+  if (!validator.isStrongPassword(password, {
+    minLength: 8,
+    maxLength: 20,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1
+  })) {
+    return res.status(422).send({ mse: 'Password must be at least 8 characters long, including uppercase and lowercase letters, numbers and symbols' });
+  }
+  if (!name) {
+    return res.status(422).send({ mse: 'name is required' });
+  }
+  if (!email) {
+    return res.status(422).send({ mse: 'email is required' });
+  }
+  if (!password) {
+    return res.status(422).send({ mse: 'password is required' });
+  }
   try {
     const hashPassword = await bcrypt.hash(password, 10);
     const userObject ={
@@ -20,13 +50,26 @@ export default async function handler(req, res) {
     const existEmail = emails.includes(email)
     if (existEmail === false) {
       const newUser = new UserModel(userObject);
-      await newUser.save();
-      res.status(201).send('sucesso!');
+      try {
+        await newUser.save();
+        const secret = process.env.SECRET
+        const token = jwt.sign({
+          name: name,
+          email: email,
+          password: hashPassword
+        },secret)
+        res.status(201).send({mse: 'sucesso!',token});
+      } catch {
+        res.status(500).send({ mse: 'Something went wrong' })
+      }
     } else {
       res.status(409).send('Esse email já foi cadastrado');
     }
   } catch (error) {
     console.error(error); // log the error for debugging purposes
-    return res.status(400).send({ err: error.message, mse: 'Something went wrong' });
+    return res.status(400).send({ mse: 'Something went wrong' });
+  }
+  finally {
+    await mongoose.connection.close();
   }
 }
