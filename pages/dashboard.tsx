9b0e675b2a17d85e61f8a1442b6d2103209bfaa6
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-13 21:07:43 -0300

    update dashboard.tsx

---
+++ pages/dashboard.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { Container, Footer, Header, Main, Title } from "../components/sharedstyles";
+
+export default function Dashboard() {
+    return(
+        <Container>
+        <Header>
+           <nav>
+               <Link href={'/home'}>Home</Link>
+               <Link href={'/register'}>Cadastre-se</Link>
+           </nav>
+       </Header>
+       <Main>
+           <Title>Faça login na sua conta.</Title>
+       </Main>
+       <Footer>
+           <Link href={'/register'}>Crie sua conta</Link>
+       </Footer>
+   </Container>
+)
+}
