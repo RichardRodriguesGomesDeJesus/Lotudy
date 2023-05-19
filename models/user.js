e39aa1236fb175b8996a7ac5033fb51cb51b3c90
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-19 11:51:37 -0300

    update  user schema

--- models/user.js
+++ models/user.js
@@ -13,8 +13,28 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  Exams:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Exam'
+  }]
+});
+
+const examSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type:String,
+    required: true,
+  },
+  questions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    text: String,
+    options: [String]
+  }]
 });
 
 export const UserModel = mongoose.models.User || mongoose.model('User', userSchema);
-
-
+export const ExamModel = mongoose.models.Exam || mongoose.model('Exam', examSchema);
