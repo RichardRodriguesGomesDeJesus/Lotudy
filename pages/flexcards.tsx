b39e613570d48f8ae4011a7f0da8b0972d93ad03
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-19 11:52:27 -0300

    create flexcards.tsx

---
+++ pages/flexcards.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Container, Footer, Header, Main, Title } from "../components/sharedstyles";
+import ValidatyToken from "../util/token_validaty";
+
+
+export default function flexcards() {
+    ValidatyToken()
+    return(
+        <Container>
+            <Header>
+                <nav>
+                    <Link href={'/exams'}>Simulados</Link>
+                    <Link href={'/flexcards'}>Flesh Cards</Link>
+                </nav>
+            </Header>
+            <Main>
+                <Title>Dashboard de ferramentas</Title>
+            </Main>
+            <Footer>
+                <Link href={'/register'}>Crie sua conta</Link>
+            </Footer>
+        </Container>
+    )
+}
