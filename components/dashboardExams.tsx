a098866f5a7bcdfb2d034ac95f468d8afa755aea
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-19 11:50:27 -0300

    update dashboardExams.tsx

---
+++ components/dashboardExams.tsx
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+import { colors } from "./sharedstyles";
+import Link from "next/link";
+
+const Dashboard = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    flex-flow: row wrap;
+    width: 80%;
+    margin: 1rem;
+
+    @media (max-width: 768px) {
+    height: auto;
+    }
+    div{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        background: ${colors.white};
+        text-align: center;
+        padding: 1rem;
+        color: inherit;
+        text-decoration: none;
+        border: 1px solid black;
+        border-radius: 10px;
+        margin: 1rem 0;
+        &:hover,
+        :focus,
+        :active {
+            cursor: pointer;
+            color: ${colors.principalColor};
+            border-color: ${colors.titleColor};
+        }
+        @media screen and (min-width: 0 ){
+            height:150px;
+            width: 150px;
+            img{
+              height: 75px;  
+              width:  75px ;
+            }
+            p{
+              font-size: 1rem;
+            }
+          };
+          @media screen and (min-width: 768px ){
+            height:200px;
+            width: 200px;
+            img{
+              height: 100px;  
+              width: 100px ;
+            }
+            p{
+              font-size: 1.5rem;
+            }
+          };
+          @media screen and (min-width: 1024px) {
+            height:250px;
+            width: 250px;
+            img{
+              height: 150px;  
+              width: 150px;
+            }
+          };
+    }
+
+`
+
+export default function DashboardExams() {
+    const listExams = [{
+        title: 'Fatec',
+        link: '/'
+    },
+    {
+        title: 'Enem',
+        link: '/'
+    },
+    {
+        title: 'Fuvest',
+        link: '/'
+    },
+    {
+        title: 'Questionarios Personalizados',
+        link: '/exams/personalized'
+    }
+    ]
+    return(
+        <>
+            <Dashboard>
+                {listExams.map( (element , index)=> (
+                    <Link href={element.link}  key={index}>
+                        <div >
+                            <p>{element.title}</p>
+                        </div>
+                    </Link>
+                ))}
+            </Dashboard>
+        </>
+    )
+}
