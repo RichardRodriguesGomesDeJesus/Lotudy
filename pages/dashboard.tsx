4202ebafa02a40dace77278e548bda421fafd90f
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-20 20:19:16 -0300

    update token validate

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,24 +1,24 @@
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { Container, Footer, Header, Main, Title } from "../components/sharedstyles";
 import ValidatyToken from "../util/token_validaty";
 
-
 export default function Dashboard() {
-    ValidatyToken()
-    return(
+    ValidatyToken();
+  return (
     <Container>
-        <Header>
-           <nav>
-               <Link href={'/exams'}>Simulados</Link>
-               <Link href={'/flexcards'}>Flesh Cards</Link>
-           </nav>
-       </Header>
-       <Main>
-           <Title>Dashboard de ferramentas</Title>
-       </Main>
-       <Footer>
-           <Link href={'/register'}>Crie sua conta</Link>
-       </Footer>
-   </Container>
-)
+      <Header>
+        <nav>
+          <Link href={'/exams'}>Simulados</Link>
+          <Link href={'/flexcards'}>Flesh Cards</Link>
+        </nav>
+      </Header>
+      <Main>
+        <Title>Dashboard de ferramentas</Title>
+      </Main>
+      <Footer>
+        <Link href={'/register'}>Crie sua conta</Link>
+      </Footer>
+    </Container>
+  );
 }
--- util/token_validaty.tsx
+++ util/token_validaty.tsx
@@ -1,30 +1,19 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import { parseCookies} from 'nookies'
-import { useEffect } from "react";
+import { parseCookies } from 'nookies';
 
-export default  function ValidatyToken() {
-    const  { 'token': token } = parseCookies()
-    const router = useRouter();
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-          if (!token) {
-            router.replace("/login");
-          } else {
-            axios
-              .post('http://localhost:3000/api/auth/verify_token', {
-                token,
-              })
-              .catch(() => {
-                router.replace("/login");
-              });
-          }
-        }, 1000); // Verifica o token a cada 60 segundos
-    
-        return () => {
-          clearInterval(interval); // Limpa o intervalo quando o componente é desmontado
-        };
-      }, []); 
-
+export default async function ValidatyToken() {
+  const { 'token': token } = parseCookies();
+  const router = useRouter();
+  if (!token) {
+      router.replace("/login");
+  } else {
+    try {
+      await axios.post('http://localhost:3000/api/auth/verify_token', {
+        token,
+      });
+    } catch (error) {
+      router.replace("/login");
+    }
+  }
 }
