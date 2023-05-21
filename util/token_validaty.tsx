ea0f05d9c22a6719d9801249300c8acf8c496c9f
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-21 20:21:37 -0300

    update token_validaty.tsx

--- util/token_validaty.tsx
+++ util/token_validaty.tsx
@@ -1,19 +1,21 @@
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 import { parseCookies } from 'nookies';
 
 export default async function ValidatyToken() {
   const { 'token': token } = parseCookies();
-  const router = useRouter();
-  if (!token) {
+  if (typeof window !== 'undefined') {
+    const router = useRouter();
+    if (!token) {
       router.replace("/login");
-  } else {
-    try {
-      await axios.post('http://localhost:3000/api/auth/verify_token', {
-        token,
-      });
-    } catch (error) {
-      router.replace("/login");
+    } else {
+      try {
+        await axios.post('http://localhost:3000/api/auth/verify_token', {
+          token,
+        });
+      } catch (error) {
+        router.replace("/login");
+      }
     }
   }
 }
