cbc297386a1baf64885048afb957ac2b5e1aa2ab
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-21 12:43:04 -0300

    update personalized.tsx

--- pages/exams/personalized.tsx
+++ pages/exams/personalized.tsx
@@ -2,10 +2,13 @@ import Link from "next/link"
 import { Container, Footer, Header, Main, Title } from "../../components/sharedstyles"
 import ValidatyToken from "../../util/token_validaty"
 import FormExams from "../../components/formExams"
+import ListExams from "../../components/listExams"
+import { useState } from "react"
 
 
 export default function personalizedExams () {
     ValidatyToken()
+    const [ formUpdate, setFormUpdate ] = useState(false)
     return(
         <>
             <Container>
@@ -17,7 +20,8 @@ export default function personalizedExams () {
             </Header>
             <Main>
                 <Title>Crie Questionarios e ordene como quiser.</Title>
-                <FormExams/>
+                <ListExams formUpdate={formUpdate} setFormUpdate={setFormUpdate}/>
+                <FormExams formUpdate={formUpdate} setFormUpdate={setFormUpdate}/>
             </Main>
             <Footer>
                 <Link href={'/register'}>Crie sua conta</Link>
