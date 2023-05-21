c08b0425d5c2d433fb1137af57b886973557a2e4
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-21 12:41:51 -0300

    update listExams.tsx

---
+++ components/listExams.tsx
@@ -0,0 +1,72 @@
+import axios from "axios";
+import { parseCookies } from "nookies";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { colors } from "./sharedstyles";
+
+const List = styled.div`
+    display: flex;
+    flex-direction: column;
+    p{
+      display: flex;
+      background: ${colors.white};
+      border: none;
+      border-radius: .5rem;
+      box-shadow: 0 4px 4px ${colors.textColor};
+      color: ${colors.textColor};
+      justify-content: center;
+      padding: 1rem;
+      width: 150px ;
+      &:hover,
+      :focus,
+      :active {
+          cursor: pointer;
+          color: ${colors.sideColor};
+          padding: calc(1rem - 1px);
+          border: 1px solid ${colors.sideColor};
+          border-color: ${colors.sideColor};
+      }
+    }
+`;
+
+export default function ListExams({ formUpdate, setFormUpdate }) {
+    const { 'token': token } = parseCookies();
+  
+    const [examList, setExamList] = useState([]);
+  
+   useEffect(() => {
+  const fetchExams = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/exams/getExams', {
+        token,
+      });
+      setExamList(response.data.list);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchExamsAndUpdateList = async () => {
+    if (token) {
+      await fetchExams();
+    }
+  };
+
+  fetchExamsAndUpdateList();
+  setFormUpdate(false)
+}, [token,formUpdate === true]);
+
+    return (
+      <>
+        <List>
+          {examList.map((exam, index) => (
+            <p key={index}>{exam}</p>
+          ))}
+          {examList.length == 0 && (
+            <p>Você ainda não possui nenhum  simulado</p>
+          )}
+        </List>
+      </>
+    );
+  }
+  
