c824dc44280f1281908cf2dea2160608ef22ade1
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-26 18:39:37 -0300

    update [exam].tsx

--- pages/exams/[exam].tsx
+++ pages/exams/[exam].tsx
@@ -1,21 +1,22 @@
 import { useRouter } from "next/router";
-import { Container, Footer, Header, Main, Title } from "../../components/sharedstyles";
+import { Button, Container, Footer, Header, Main, Title } from "../../components/sharedstyles";
 import Link from "next/link";
 import axios from "axios";
 import { parseCookies } from "nookies";
 import { useState, useEffect } from "react";
 import QuestionForm from "../../components/questionForm";
+import Question from "../../components/question";
 
 export default function ExamPage() {
 
   const router = useRouter();
   const { asPath } = router;
-  const parts = asPath.split('/'); // Divide a URL em partes separadas por '/'
+  const parts = asPath.split('/'); 
 
-  // Obtém o último elemento da divisão, que é o nome do exame
   const examName = parts[parts.length - 1];
   const { 'token': token } = parseCookies();
   const [userAuth , setUserAuth] = useState(true)
+  const [question, setQuestion] = useState(false)
   useEffect(()=>{
     if (!token) {
       setUserAuth(false)
@@ -34,6 +35,7 @@ export default function ExamPage() {
   if (userAuth === false) {
     router.push("/login");
   }
+  const [form, setForm] = useState(false)
   return (
     <Container>
     <Header>
@@ -43,8 +45,28 @@ export default function ExamPage() {
       </nav>
     </Header>
     <Main>
-      <Title> Crie questões para o simulado {examName}</Title>
-      <QuestionForm/>
+      <Title> Crie questões para o simulado {examName} ou faça ele</Title>
+      {form === true &&
+        <QuestionForm examName={examName} token={token} setForm={setForm} setQuestion={setQuestion} question={question} />
+      }
+      {
+        form === false &&
+        <Button onClick={(e)=>{
+          e.preventDefault()
+          setForm(true)
+        }}>Criar questões</Button>
+      }
+      {
+        question === false && form === false && 
+        <Button onClick={(e)=>{
+          e.preventDefault()
+          setQuestion(true)
+        }}>Começar simulado</Button>
+      }
+      {
+        question === true &&   form === false &&
+          <Question/>
+      }
     </Main>
     <Footer>
       <Link href={'/register'}>Crie sua conta</Link>
