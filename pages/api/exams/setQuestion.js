6cbb032f673be1e7e9b44bf36626b66276112b40
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-05-28 22:02:42 -0300

    update setQuestion.js

--- pages/api/exams/setQuestion.js
+++ pages/api/exams/setQuestion.js
@@ -8,25 +8,31 @@ export default async function setQuestion(req, res) {
         if (req.method !== 'PUT') {
             return res.status(405).send({ mse: 'Method Not Allowed' });
           }
-          const { token  , options , text, title } = req.body;
+          const { token  , options , text, title, correctOption } = req.body;
 
           if (!token) {
             return res.status(403).send('A token is required');
           }
           if (!options) {
-            return res.status(403).send('A token is required');
+            return res.status(403).send(' options is required');
           }
           if (!text) {
-            return res.status(403).send('A token is required');
+            return res.status(403).send(' text is required');
           }
           if (!title) {
-            return res.status(403).send('A token is required');
+            return res.status(403).send(' title is required');
+          }
+          if (!correctOption) {
+            return res.status(403).send(' correctOption is required');
           }
 
           const decoded = jwt.verify(token, process.env.SECRET);
 
           if (!decoded.userId) {
             return res.status(401).send({ error: 'Invalid token'});
+          }
+          if (options.length < 2) {
+            return res.status(403).send(' options is invalid');
           }
           await connectMongo();
           const exams = await ExamModel.find({ });
@@ -39,6 +45,7 @@ export default async function setQuestion(req, res) {
             id: new mongoose.Types.ObjectId(),
             text: text,
             options: options,
+            correctOption
           };
 
           exam.questions.push(newQuestion);
