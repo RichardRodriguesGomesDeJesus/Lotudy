726875e925b1ca722ad625e144d4b00a633dd48b
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-07-10 11:27:34 -0300

    update listExams.tsx

--- components/listExams.tsx
+++ components/listExams.tsx
@@ -2,22 +2,29 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { colors } from "./sharedstyles";
+import { Button, ButtonClose, colors } from "./sharedstyles";
 import Link from "next/link";
 
 const List = styled.div`
+    align-items: center;
     display: flex;
     flex-direction: column;
-    p{
+    gap: 1rem;
+    padding: 1rem;
+    div,a{
+      box-shadow: 0 4px 20px ${colors.textColor};
       display: flex;
-      background: ${colors.white};
-      border: none;
-      border-radius: .5rem;
-      box-shadow: 0 4px 4px ${colors.textColor};
-      color: ${colors.textColor};
+      flex-direction: column;
       justify-content: center;
+      align-items: center;
+      background: ${colors.white};
+      text-align: center;
       padding: 1rem;
-      width: 150px ;
+      color: inherit;
+      text-decoration: none;
+      border-radius: 10px;
+      height:200px;
+      width: 200px;
       &:hover,
       :focus,
       :active {
@@ -34,6 +41,7 @@ export default function ListExams({ formUpdate, setFormUpdate }) {
   const { 'token': token } = parseCookies();
   
   const [examList, setExamList] = useState([]);
+  const [edit, setEdit] = useState(false)
   
   useEffect(() => {
     const fetchExams = async () => {
@@ -56,17 +64,50 @@ export default function ListExams({ formUpdate, setFormUpdate }) {
   fetchExamsAndUpdateList();
   setFormUpdate(false)
   }, [token,formUpdate === true]);
-
+  function Delete(exam) {
+    axios.put('api/exams/putExam',{
+      token,
+      title: exam
+    })
+    .then((res)=>setFormUpdate(true))
+    .catch((err)=>console.log(err))
+    
+  }
     return (
       <>
         <List>
-          {examList.map((exam, index) => (
-            <Link href={`/exams/${encodeURIComponent(exam)}`} key={index}>
-              <p>{exam}</p>
-            </Link>
-          ))}
+          {edit === false&&(
+            examList.map((exam, index) => (
+              <Link href={`/exams/${encodeURIComponent(exam)}`} key={index}>
+                <p>{exam}</p>
+              </Link>
+            ))
+          )}
+          {edit === true&&(
+            examList.map((exam,index)=>(
+              <span key={index}>
+                <ButtonClose onClick={()=>{
+                  setEdit(false)
+                  Delete(exam)
+                }}><img src="/icons/close.png" alt="delete" /></ButtonClose>
+                <div>
+                  <p>{exam}</p>
+                </div>
+              </span>
+            ))
+          )}
           {examList.length == 0 && (
-            <p>Você ainda não possui nenhum  simulado</p>
+            <p>You don't have any exams yet</p>
+          )}
+          {examList.length > 0 && edit === false &&(
+            <Button onClick={()=>{
+              setEdit(true)
+            }} >edit</Button>
+          )}
+          {examList.length > 0 && edit === true &&(
+            <Button onClick={()=>{
+              setEdit(false)
+            }} >finish editing</Button>
           )}
         </List>
       </>
