db490c5798078e37c077866aa9ebb03e159d38c6
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-07-10 21:23:29 -0300

    update [exam].tsx

--- pages/exams/[exam].tsx
+++ pages/exams/[exam].tsx
@@ -78,6 +78,7 @@ export default function ExamPage() {
         <Link href={'/dashboard'} > Dashboard</Link>
         <Link href={'/exams'}>Exams</Link>
         <Link href={'/study-cycle'}>Study Cycle</Link>
+        <Link href={'/flex-cards'}>Flash Cards</Link>
       </nav>
     </Header>
     <Main>
